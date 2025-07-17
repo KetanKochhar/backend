c1a2472a7f7c887252a38da945eba7e4e8f9dd34
SunilSihag0807 <sunil.123028@marwadiuniversity.ac.in>
2025-07-17 23:54:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.get('/maintenance', (req, res) => {
   res.render('maintenance');
 });
 
-app.use((req, res, next) => {
-    return res.redirect('/maintenance');
-});
+// app.use((req, res, next) => {
+//     return res.redirect('/maintenance');
+// });
 
 //creating the sessiona and haldling the sesion for 1day jab tak login rahega user ke pc mein uska account
 app.use(session({
